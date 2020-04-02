2f672c6ff7e57a4432a7a75adb2804123b6effe8
Felipe Arias <felipearias@MacBook-Pro-de-Felipe.local>
2020-04-01 21:52:24 -0500

    cambios bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 
+
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuPrincipalComponent
+    MenuPrincipalComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+   
+   
+    
+    BrowserAnimationsModule
+   
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
+
 export class AppModule { }
